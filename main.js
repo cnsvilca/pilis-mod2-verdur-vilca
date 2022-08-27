da914e79d41edc7b0fa3608ad984ce0d1492f9d8
carlos vilca <carlosvilca.juy@gmail.com>
2022-08-27 00:00:06 -0300

    se agrego codigo JS seccion clima

--- main.js
+++ main.js
@@ -37,3 +37,55 @@ function cleanForm() {
 
 let boton = document.getElementById("enviar");
 boton.addEventListener("click", onClick);
+
+// ------------------seccion clima----------------- 
+
+async function obtenerClimaDia() {
+    try {
+        let respuesta = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=-24.182673866114143&lon=-65.33121206931652&appid=7e0e756c0bb5247d4b74e950b8e8b8b2&units=metric&lang=es')
+        let clima = await respuesta.json();
+        let { main, name, weather } = clima;
+        document.getElementById('clima-grados').textContent = Math.round(main.temp) + '°C';
+        document.getElementById('clima-img').src = 'http://openweathermap.org/img/wn/' + weather[0].icon + '@2x.png';
+        document.getElementById('clima-feelslike').textContent = "Sensacion termiaca: " + Math.round(main.feels_like) + "°C";
+        document.getElementById('clima-humedad').textContent = "Humedad: " + main.humidity + '%';
+        document.getElementById('clima-name').textContent = name;
+        document.getElementById('clima-desc').textContent = weather[0].description
+    } catch {
+        console.log("no se pudo obtener clima: ")
+    }
+}
+obtenerClimaDia()
+
+async function obtenerClima() {
+    try {
+        let respuesta = await fetch('http://api.openweathermap.org/data/2.5/forecast?lat=-24.182673866114143&lon=-65.33121206931652&appid=bf94524c67d2c84b740df27ed1f3674d&units=metric&lang=es')
+        let climas = await respuesta.json();
+        let lista = obtenerListaDatosDias(climas)
+        let dias = ['domingo', 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado']
+        let i = 1;
+        for (let index = 0; index < lista.length; index++) {
+            for (const dia of lista) {
+                document.getElementById(`dia-${i}-nombre`).textContent = dias[new Date(dia.dt_txt).getDay()];
+                document.getElementById(`dia-${i}-img`).src = 'http://openweathermap.org/img/wn/' + dia.weather[0].icon + '@2x.png';
+                document.getElementById(`dia-${i}-temp`).textContent = Math.round(dia.main.temp) + '°C'
+                document.getElementById(`dia-${i}-descrip`).textContent = dia.weather[0].description;
+                i += 1;
+            }
+        }
+    } catch {
+        console.log("no se pudo obtener clima")
+    }
+}
+obtenerClima()
+
+function obtenerListaDatosDias(climas) {
+    let lista = [];
+    for (let index = 0; index < climas.list.length; index++) {
+        let hora = new Date(climas.list[index].dt_txt).getHours()
+        if (hora == 15) {
+            lista.push(climas.list[index])
+        }
+    }
+    return lista;
+}
